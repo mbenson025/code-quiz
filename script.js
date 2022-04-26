028299b49584101edbcda8b347156de48dce5c37
mbenson025 <mbenson025@gmail.com>
2022-04-26 17:04:01 -0500

    adding functional time and quiz

--- script.js
+++ script.js
@@ -1,76 +1,91 @@
-// normal variables
-var score = 0;
+// global variables
+let score = 0;
+let time = 75;
 var questionDisplay = 0;
+var gameRunning;
 
-// js variables
-
+// DOM variables
 var quiz = document.getElementById('quiz');
 var win = document.getElementById('win');
 var lose = document.getElementById('lose');
 var begin = document.getElementById('begin');
-var timer = document.getElementById('timer');
+var timeClock = document.getElementById('time');
 var startpage = document.getElementById('startpage');
 var quizmain = document.getElementById('quiz-main');
 var answer = document.getElementById('answer');
 
-var timeLeft = 80;
-var right = 5;
-var wrong = 10;
+
+var question = document.querySelector('#question')
 
 
-var question = 0;
+
+
 
 // quiz questions
-// let quizQuestions = [
-//   {
-//     question: 'Which word represents the absence of a value?',
-//     choices: [ 'Variable','null', 'Operator', 'JavaScript' ],
-//     correctchoice: 'null'
+let quizQuestions = [
+  {
+    question: 'Which word represents the absence of a value?',
+    choices: ['Variable','null', 'Operator', 'JavaScript'],
+    correctchoice: 'null'
 
-//   },
-//   {
-//     question 'What can a "loop" be used for in Javascript?',
-//     choices: [ 'Storing data on a browser window', 'Changing a string to a number', 'Perform repeated tasks based on a condition', 'Downloading a file' ],
-//     correctchoice: 'Perform repeated tasks based on a condition',
+  },
+  {
+    question: 'What can a "loop" be used for in Javascript?',
+    choices: ['Storing data on a browser window', 'Changing a string to a number', 'Perform repeated tasks based on a condition', 'Downloading a file'],
+    correctchoice: 'Perform repeated tasks based on a condition',
 
-//   },
-//   {
-//     question 'What is the name of a series of characters inside a set of quotes?',
-//     choices: [ 'string', 'yarn', 'function', 'hot dog' ],
-//     correctchoice: 'string'
+  },
+  {
+    question: 'What is the name of a series of characters inside a set of quotes?',
+    choices: ['string', 'yarn', 'function', 'hot dog'],
+    correctchoice: 'string'
 
-//   },
-//   {
-//     question 'Which method removes the last element of an array?',
-//     choices: [ 'pop()', 'sort()', 'valueOf()', 'join()' ],
-//     correctchoice: 'pop()'
+  },
+  {
+    question: 'Which method removes the last element of an array?',
+    choices: ['pop()', 'sort()', 'valueOf()', 'join()'],
+    correctchoice: 'pop()'
 
-//   },
-//   {
-//     question 'What is "concatenation" in coding?',
-//     choices: [ 'Sorting alphabetically', 'Writing information to the browser console', 'Copying a line of code', 'Merging two or more strings' ],
-//     correctchoice: 'Merging two or more strings'
+  },
+  {
+    question: 'What is "concatenation" in coding?',
+    choices: ['Sorting alphabetically', 'Writing information to the browser console', 'Copying a line of code', 'Merging two or more strings'],
+    correctchoice: 'Merging two or more strings'
 
-//   },
-//   {
-//     question 'How long did it take to develop JavaScript?',
-//     answer1: [ '4 years', '2 years', '10 days', '9 months'],
-//     correctchoice: '10 days',
+  },
+  {
+    question: 'How long did it take to develop JavaScript?',
+    choices: ['4 years', '2 years', '10 days', '9 months'],
+    correctchoice: '10 days',
 
-//   },
-// ];
-
-
+  },
+];
 
 
 
 
-// function startTimer();
-//   timer.textContent = time;
-//   time--;
+
+
 
 function startQuiz() {
+setInterval(countdown, 1000)
+}
 
+//pick a question
+function displayQuestion(quizQuestions) {
+  let index = Math.floor(Math.random() * quizQuestions.length)
+
+  question.innerHTML = quizQuestions[index];
+}
+
+function countdown() {
+  if(time>0) {
+    time--;
+  } else if(time <= 0) {
+    //GAME OVER-----
+    gameRunning = false;
+  }
+  timeClock.innerHTML = time;
 }
 
 
@@ -87,6 +102,7 @@ begin.addEventListener('click', function(e) {
 
   startQuiz();
   // startTimer();
+  countdown
 })
 
 
