978fbadcd8d77913b5597d8a9c0f3faadfa77f87
mbenson025 <mbenson025@gmail.com>
2022-04-26 16:07:31 -0500

    second commit

--- script.js
+++ script.js
@@ -1,73 +1,61 @@
 // normal variables
 var score = 0;
+var questionDisplay = 0;
 
 // js variables
 
 var quiz = document.getElementById('quiz');
 var win = document.getElementById('win');
 var lose = document.getElementById('lose');
-var begin = document.getElementById('.begin');
-var timer = document.getElementById('.timer');
-var startpage = document.getElementById('.startpage');
-var quizmain = document.getElementById('.quiz-main');
+var begin = document.getElementById('begin');
+var timer = document.getElementById('timer');
+var startpage = document.getElementById('startpage');
+var quizmain = document.getElementById('quiz-main');
+var answer = document.getElementById('answer');
+
+var timeLeft = 80;
+var right = 5;
+var wrong = 10;
 
 
+var question = 0;
 
-
-//quiz questions
+// quiz questions
 // let quizQuestions = [
 //   {
-//     question 'Which word represents the absence of a value?',
-//     answer1: 'Variable',
-//     answer2: 'null',
-//     answer3: 'Operator',
-//     answer4: 'JavaScript',
-//     correctchoice: 'answer2',
+//     question: 'Which word represents the absence of a value?',
+//     choices: [ 'Variable','null', 'Operator', 'JavaScript' ],
+//     correctchoice: 'null'
 
 //   },
 //   {
 //     question 'What can a "loop" be used for in Javascript?',
-//     answer1: 'Storing data on a browser window',
-//     answer2: 'Changing a string to a number',
-//     answer3: 'Perform repeated tasks based on a condition',
-//     answer4: 'Downloading a virus',
-//     correctchoice: 'answer3',
+//     choices: [ 'Storing data on a browser window', 'Changing a string to a number', 'Perform repeated tasks based on a condition', 'Downloading a file' ],
+//     correctchoice: 'Perform repeated tasks based on a condition',
 
 //   },
 //   {
 //     question 'What is the name of a series of characters inside a set of quotes?',
-//     answer1: 'string',
-//     answer2: 'yarn',
-//     answer3: 'function',
-//     answer4: 'hot dog',
-//     correctchoice: 'answer1',
+//     choices: [ 'string', 'yarn', 'function', 'hot dog' ],
+//     correctchoice: 'string'
 
 //   },
 //   {
 //     question 'Which method removes the last element of an array?',
-//     answer1: 'pop()',
-//     answer2: 'sort()',
-//     answer3: 'valueOf()',
-//     answer4: 'join()',
-//     correctchoice: 'answer1',
+//     choices: [ 'pop()', 'sort()', 'valueOf()', 'join()' ],
+//     correctchoice: 'pop()'
 
 //   },
 //   {
 //     question 'What is "concatenation" in coding?',
-//     answer1: 'Sorting alphabetically',
-//     answer2: 'Writing information to the browser console',
-//     answer3: 'Copying a line of code',
-//     answer4: 'Merging two or more strings',
-//     correctchoice: 'answer4',
+//     choices: [ 'Sorting alphabetically', 'Writing information to the browser console', 'Copying a line of code', 'Merging two or more strings' ],
+//     correctchoice: 'Merging two or more strings'
 
 //   },
 //   {
 //     question 'How long did it take to develop JavaScript?',
-//     answer1: '4 years',
-//     answer2: '2 years',
-//     answer3: '10 days',
-//     answer4: '9 months',
-//     correctchoice: 'answer3',
+//     answer1: [ '4 years', '2 years', '10 days', '9 months'],
+//     correctchoice: '10 days',
 
 //   },
 // ];
@@ -76,37 +64,34 @@ var quizmain = document.getElementById('.quiz-main');
 
 
 
-function beginGame() {
 
-    timer = 75
+// function startTimer();
+//   timer.textContent = time;
+//   time--;
 
-    startpage.style.display = "none";
-    quizmain.style.display = "block";
-
-
+function startQuiz() {
 
 }
 
 
 
+// eventListeners
+
+//start page click to quiz screen
+begin.addEventListener('click', function(e) {
+
+  startpage.style.display = 'none';
+  quizmain.style.display = 'flex';
+
+  // console.log('test');
+
+  startQuiz();
+  // startTimer();
+})
 
 
 
 
-//functions
-// function countDown() {
-//   timer = 75
-
-// var 
-
-
-// }
-
-
-
-
-//eventListeners
-start.addEventListener('click', beginGame)
 
 
 
